517b511922952befee823ccbc51a04e75133da2f
dingyuebo <dingyuebo@dingyuebodeMacBook-Pro.local>
2026-07-06 20:33:35 +0800

    qm

--- mobile/rn-app/src/store/globalStore.js
+++
@@ -1,9 +0,0 @@
-import {create} from 'zustand';
-
-export const useGlobalStore = create(set => ({
-  loading: false,
-  selectedDate: '',
-
-  setLoading: loading => set({loading}),
-  setSelectedDate: date => set({selectedDate: date}),
-}));
